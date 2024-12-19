8b84266b0550ebf0cc23bef62822bf000f425313
rustmadeeasy-medium <rustmadeeasy@proton.me>
2024-12-19 16:55:00 +0100

    Integrating a form and Bootstrap styling

--- dist/course-rust-front-end-e468bea3a57fb41d.js
+++ dist/course-rust-front-end-e468bea3a57fb41d.js
@@ -558,11 +558,11 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2303 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2304 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 149, __wbg_adapter_20);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper2990 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2991 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 214, __wbg_adapter_23);
         return ret;
     };
--- src/pages/login.rs
+++ src/pages/login.rs
@@ -3,12 +3,18 @@ use yew::prelude::*;
 #[function_component(Login)]
 pub fn login() -> Html {
     html! {
-        <form>
-            <label> {"Username"} </label>
-            <input type = "text" />
-            <label> {"Password"} </label>
-            <input type = "text" />
-            <button type = "submit"> {"Login"} </button>
-        </form>
+        <div class = "container">
+            <div class = "row min-vh-100 justify-content-center align">
+                <div class = "col-md-4">
+                    <form>
+                        <label> {"Username"} </label>
+                        <input type = "text" />
+                        <label> {"Password"} </label>
+                        <input type = "text" />
+                        <button type = "submit"> {"Login"} </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     }
 } 
